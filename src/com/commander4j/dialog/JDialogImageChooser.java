63665780c8565e8ee293ff827975e741178f49a9
David Garratt <support@commander4j.com>
2026-05-16 21:06:05 +0100

    Misc Update

--- src/com/commander4j/dialog/JDialogImageChooser.java
+++ src/com/commander4j/dialog/JDialogImageChooser.java
@@ -30,7 +30,7 @@ public class JDialogImageChooser extends JDialog {
     public JDialogImageChooser(Frame owner, File folder) {
         super(owner, "Select an Image", true);
         setResizable(false);
-        
+
         getContentPane().setLayout(new BorderLayout());
         getContentPane().setBackground(Common.color_app_window);
 
